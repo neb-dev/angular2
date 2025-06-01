7f2989b3442fbe51ad50b73a05fea19583cf1c90
neb <jneb.dev@outlook.com>
2025-05-31 22:12:12 -0600

    fixed app spec, adds karma config, adds test yml for CI

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular2' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular2');
-  });
+  // it(`should have the 'angular2' title`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('angular2');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular2');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Investment Calculator'
+    );
   });
 });
