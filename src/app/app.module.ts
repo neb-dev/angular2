c7627e1a0d4ec3cedb54882dd47005141700f9e9
neb <jneb.dev@outlook.com>
2025-06-03 11:00:27 -0600

    adds commented out angular module implementation

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+// import { NgModule } from '@angular/core';
+// import { AppComponent } from './app.component';
+// import { HeaderComponent } from './header/header.component';
+// import { InvestmentService } from './services/investment.service';
+// import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+// import { UserInputModule } from "./user-input/user-input.module";
+// import { BrowserModule } from '@angular/platform-browser';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HeaderComponent,
+//     InvestmentResultsComponent,
+//   ],
+//   imports: [BrowserModule, UserInputModule],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
